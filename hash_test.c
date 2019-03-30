180d547915da92b9921fed0a4265feadb72b0461
fuyousheng <123456>
2019-03-30 18:21:07 +0800

    hash

--- hash_test.c
+++ hash_test.c
@@ -14,17 +14,34 @@
       return  hash_code;
   }
 int main(int argc,char*argv[]) {
-    HashArray hashArray;
-    hash_init_ex(&hashArray, hash_function, 100, 0.72,1024,false);
+    HashArray pHash;
+    int result = 0;
+    if ((result = hash_init_ex(&pHash, PJWHash, 2 * 1024, 0.75, 0, true)) != 0) {
+
+        printf("file: "__FILE__", line: %d, " \
+            "hash_init_ex fail, errno: %d, error info: %s", \
+            __LINE__, result, STRERROR(result));
+        return result;
+    }
     char *key = "java";
     char *value = "javax";
-    hash_insert_ex(&hashArray, key, strlen(key), value,strlen(value),false);
-    char to[20];
-    int len = 0;
-    memset(to,0,20);
-    hash_get(&hashArray, key, strlen(key), to, &len);
 
-    printf("get values=%s\n", to);
-    hash_destroy(&hashArray);
-    return 0;
-}
+
+    if ((result=hash_insert_ex(&pHash, key,
+				strlen(key)+1, value,
+				strlen(value)+1, true)) < 0) {
+        printf("file: "__FILE__", line: %d, " \
+            "hash_init_ex fail, errno: %d, error info: %s", \
+            __LINE__, result, STRERROR(result));
+        return result;
+    }
+        char to[20];
+        int len = 20;
+        memset(to, 0, 20);
+
+        hash_get(&pHash, key, strlen(key) + 1, to, &len);
+
+        printf("get values=%s ret=%d \n", to, result);
+        hash_destroy(&pHash);
+        return 0;
+    }
