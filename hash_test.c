ad0849cb1f0f22a105d004ae9037e06e1fb7ccc1
hcmfys@163.com <hcmfys@163.com>
2019-03-31 22:25:55 +0800

    c

--- hash_test.c
+++ hash_test.c
@@ -3,45 +3,60 @@
 //
 
 #include "hash.h"
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-  int hash_function(const void *key, const int key_len) {
-      printf("%s size=%d\n", (char *) key, key_len);
-      int hash_code= SDBMHash(key, key_len);
-      printf("hash_code=%d\n",hash_code);
-      return  hash_code;
-  }
-int main(int argc,char*argv[]) {
+/**
+ *
+ * @param key
+ * @param key_len
+ * @return
+ */
+int hash_function(const void *key, const int key_len) {
+    printf("%s size=%d\n", (char *) key, key_len);
+    int hash_code = SDBMHash(key, key_len);
+    printf("hash_code=%d\n", hash_code);
+    return hash_code;
+}
+
+struct goods {
+    long goods_id;
+    int goods_name_length;
+    char *goods_name;
+};
+
+int main(int argc, char *argv[]) {
     HashArray pHash;
     int result = 0;
     if ((result = hash_init_ex(&pHash, PJWHash, 2 * 1024, 0.75, 0, true)) != 0) {
 
-        printf("file: "__FILE__", line: %d, " \
-            "hash_init_ex fail, errno: %d, error info: %s", \
-            __LINE__, result, STRERROR(result));
+        printf("file: "__FILE__", line: %d, "
+               "hash_init_ex fail, errno: %d, error info: %s",
+               __LINE__, result, STRERROR(result));
         return result;
     }
-    char *key = "java";
-    char *value = "javax";
 
 
-    if ((result=hash_insert_ex(&pHash, key,
-				strlen(key)+1, value,
-				strlen(value)+1, true)) < 0) {
-        printf("file: "__FILE__", line: %d, " \
-            "hash_init_ex fail, errno: %d, error info: %s", \
-            __LINE__, result, STRERROR(result));
+    struct goods food_goods;
+    food_goods.goods_id = 12345;
+    food_goods.goods_name = "我爱中国人";
+    food_goods.goods_name_length = strlen(food_goods.goods_name) + 1;
+
+    int len = sizeof(struct goods) + food_goods.goods_name_length * sizeof(char) + 1;
+
+    if ((result = hash_insert_ex(&pHash, &food_goods.goods_id,
+                                 sizeof(food_goods.goods_id), &food_goods,
+                                 sizeof(food_goods), true)) < 0) {
+        printf("file: "__FILE__", line: %d, "
+               "hash_init_ex fail, errno: %d, error info: %s",
+               __LINE__, result, STRERROR(result));
         return result;
     }
-        char to[20];
-        int len = 10;
-        memset(to, 0, 20);
 
-        hash_get(&pHash, key, strlen(key) + 1, to, &len);
-
-        printf("get values=%s ret=%d \n", to, result);
-        hash_destroy(&pHash);
-        return 0;
-    }
+    struct goods foot;
+    printf("%d -%d -%d \n", sizeof(long), sizeof(char*), sizeof(int));
+    hash_get(&pHash, &food_goods.goods_id, sizeof(food_goods.goods_id), &foot, &len);
+    printf("get values=%s ret=%d \n", foot.goods_name, result);
+    hash_destroy(&pHash);
+    return 0;
+}
